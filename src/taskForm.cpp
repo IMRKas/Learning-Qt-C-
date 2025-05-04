db0e9a99856084d525a6ed87c82327a3f1fe37f3
Igor da Mota Rabelo <igormotarabelo2@gmail.com>
2025-05-04 15:31:48 -0300

    taskForm.cpp 1º Completed

--- src/taskForm.cpp
+++ src/taskForm.cpp
@@ -1,6 +1,15 @@
 #include "TaskForm.h"
 #include <QDate>
 #include <QMessageBox>
+#include <QSqlQuery>
+#include <QSqlError>
+
+
+namespace Status {
+	const QString Pending = "Pendente";
+	const QString Completed = "Concluída";
+	const QString InProgress = "Em andamento";
+}
 
 TaskForm::TaskForm(QWidget* parent) : QWidget(parent){
 	setWindowTitle("Nova Tafera");
@@ -46,25 +55,44 @@ TaskForm::TaskForm(QWidget* parent) : QWidget(parent){
 		connect(saveButton, &QPushButton::clicked, this, [=](){
 				if(taskTitle->text().isEmpty()){
 					QMessageBox::information(this, "Atenção", "O campo \"Título\" não pode ficar em branco");
-				}
+				} else {
 
+					QSqlQuery insertQuery;
+					insertQuery.prepare("INSERT INTO tasks(title, description, due_date, status) VALUES (?, ?, ?, ?);");
+					insertQuery.addBindValue(taskTitle->text());
+					insertQuery.addBindValue((taskDescription->toPlainText().isEmpty()) ? QVariant(QVariant::String) : QVariant(taskDescription->toPlainText()));
+					insertQuery.addBindValue(taskDL->date().toString("yyyy-MM-dd"));
+					insertQuery.addBindValue(taskStatus->currentData().toString());
+
+					if(!insertQuery.exec()){
+						QMessageBox::critical(this, "ERRO", "Erro ao adicionar tarefa:\n" + insertQuery.lastError().text());
+						} else {
+							QMessageBox::information(this, "Sucesso", "Tarefa adicionada com sucesso.");
+							this->close();
+						}
+				}
 				});
 
 
-
 	// Aux Settings
-		// Current Date
+		// Date Setting
 		QDate currentDate = QDate::currentDate();
 		taskDL->setDate(currentDate);
 		taskDL->setCalendarPopup(true);
+		taskDL->setDisplayFormat("dd/MM/yyyy");
+		taskDL->setLocale(QLocale(QLocale::Portuguese, QLocale::Brazil));
 
 		// ComboBox
-		taskStatus->addItems({"Pendente","Concluída","Em andamento"});
+		taskStatus->addItem(Status::Pending, "pendente");
+		taskStatus->addItem(Status::Completed, "concluida");
+		taskStatus->addItem(Status::InProgress, "em_andamento");
 
-		// dateStatusRow
+			// dateStatusRow
 			comboBox = new QLabel("Status");
 			dateStatusRow->addWidget(taskDL);
 			dateStatusRow->addWidget(comboBox);
 			dateStatusRow->addWidget(taskStatus);
+
+	taskTitle->setFocus();
 
 }
