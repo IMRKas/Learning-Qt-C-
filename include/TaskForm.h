e7a2a782dd71b6d7ee9224108693a9b8f60ba8b7
Igor da Mota Rabelo <igormotarabelo2@gmail.com>
2025-05-04 14:10:10 -0300

    TaskForm Window Layout completed. Still need sql logic.

--- include/TaskForm.h
+++ include/TaskForm.h
@@ -1,10 +1,46 @@
 #ifndef TASK_FORM_H
 #define TASK_FORM_H
 
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QComboBox>
+#include <QDateEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFormLayout>
+#include <QLabel>
 
 
+class QPushButton;
 
+class TaskForm : public QWidget {
+	Q_OBJECT
+	public:
+		//Constructors
+		TaskForm(QWidget* parent = nullptr);
 
+	
+	private:
 
+		// Layouts
+		QVBoxLayout* mainLayout;
+		QHBoxLayout* buttonsRow;
+		QFormLayout* taskForm;
+		QHBoxLayout* dateStatusRow;
 
+		//Buttons
+		QPushButton* saveButton;
+		QPushButton* cancelButton;
+
+		// Form Fields
+		QLineEdit* taskTitle;
+		QTextEdit* taskDescription;
+		QDateEdit* taskDL;
+		QComboBox* taskStatus;
+
+		// Labels
+		QLabel* comboBox;
+};
 #endif
--- src/taskForm.cpp
+++ src/taskForm.cpp
@@ -1,1 +1,70 @@
 #include "TaskForm.h"
+#include <QDate>
+#include <QMessageBox>
+
+TaskForm::TaskForm(QWidget* parent) : QWidget(parent){
+	setWindowTitle("Nova Tafera");
+	setAttribute(Qt::WA_DeleteOnClose);
+
+	// Buttons
+	saveButton = new QPushButton("Salvar", this);
+	cancelButton = new QPushButton("Cancelar", this);
+
+	// Layouts
+	mainLayout = new QVBoxLayout(this);
+	buttonsRow = new QHBoxLayout;
+	taskForm = new QFormLayout;
+	dateStatusRow = new QHBoxLayout;
+
+
+	// Form Fields
+	taskTitle = new QLineEdit(this);
+	taskDescription = new QTextEdit(this);
+	taskDL = new QDateEdit(this);
+	taskStatus = new QComboBox(this); // Pending, completed, in progress
+
+
+	// FormLayout
+	taskForm->addRow("Título", taskTitle);
+	taskForm->addRow("Descrição",taskDescription);
+	taskForm->addRow("Data Limite", dateStatusRow);
+
+
+	// Window Layout
+	mainLayout->addLayout(taskForm);
+	mainLayout->addLayout(buttonsRow);
+	buttonsRow->addWidget(saveButton);
+	buttonsRow->addWidget(cancelButton);
+
+
+
+	// Connects
+		// CancelButton
+		connect(cancelButton, &QPushButton::clicked, this, &QWidget::close);
+
+		// saveButton
+		connect(saveButton, &QPushButton::clicked, this, [=](){
+				if(taskTitle->text().isEmpty()){
+					QMessageBox::information(this, "Atenção", "O campo \"Título\" não pode ficar em branco");
+				}
+
+				});
+
+
+
+	// Aux Settings
+		// Current Date
+		QDate currentDate = QDate::currentDate();
+		taskDL->setDate(currentDate);
+		taskDL->setCalendarPopup(true);
+
+		// ComboBox
+		taskStatus->addItems({"Pendente","Concluída","Em andamento"});
+
+		// dateStatusRow
+			comboBox = new QLabel("Status");
+			dateStatusRow->addWidget(taskDL);
+			dateStatusRow->addWidget(comboBox);
+			dateStatusRow->addWidget(taskStatus);
+
+}
